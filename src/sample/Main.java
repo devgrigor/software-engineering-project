fd06866cf2d441d6574a0c4bf437ae350975e58a
1Monika <monikastepanyan@gmail.com>
2019-09-22 13:54:16 +0400

    Simple FileChooser

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,74 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.geometry.*;
+import javafx.scene.paint.*;
+import javafx.scene.canvas.*;
+import javafx.scene.text.*;
+import javafx.scene.Group;
+import javafx.scene.shape.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.collections.*;
+import java.io.*;
+import javafx.stage.FileChooser;
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+
+    public void start(Stage stage) throws Exception
+    {
+
+        try {
+
+
+            stage.setTitle("FileChooser");
+
+            FileChooser fil_chooser = new FileChooser();
+
+            Label label = new Label("no files selected");
+
+            Button button = new Button("Select");
+
+            EventHandler<ActionEvent> event =
+                    new EventHandler<ActionEvent>() {
+
+                        public void handle(ActionEvent e)
+                        {
+                            File file = fil_chooser.showOpenDialog(stage);
+
+                            if (file != null) {
+
+                                label.setText(file.getAbsolutePath()
+                                        + "  selected");
+                            }
+                        }
+                    };
+
+            button.setOnAction(event);
+
+            VBox vbox = new VBox(30, label, button);
+
+            vbox.setAlignment(Pos.CENTER);
+
+            Scene scene = new Scene(vbox, 800, 500);
+
+            stage.setScene(scene);
+
+            stage.show();
+        }
+
+        catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
     }
 
 
